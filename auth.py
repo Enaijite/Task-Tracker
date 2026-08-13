f6fb234617351d2c00dcaefa93def93288d52a47
Enaijite024 <220177245@aston.ac.uk>
2026-08-13 13:00:41 +0100

    Complete Auth File, Understanding of it needs to deepen

---
+++ auth.py
@@ -0,0 +1,49 @@
+from time import timezone
+
+import jwt
+import datetime
+from flask import Blueprint, request, jsonify, current_app
+from werkzeug.security import generate_password_hash, check_password_hash
+from models import db, User
+
+auth_bp = Blueprint("auth", __name__)
+
+@auth_bp.route('/register', methods=['POST'])
+def register():
+    data = request.get_json()
+
+    if not data or not data.get('username') or not data.get('password'):
+        return jsonify({'error':'password or username required'}), 400
+
+    existing_user = User.query.filter_by(username=data['username']).first()
+
+    if existing_user:
+        return jsonify({'error':'Username already taken'}), 409
+
+    hashed_password = generate_password_hash(data['password'])
+    new_user = User(username=data['username'], password_hashed=hashed_password)
+
+    db.session.add(new_user)
+    db.session.commit()
+
+    return jsonify({'message':'new user created successfully'}), 201
+
+@auth_bp.route('/login', methods=['POST'])
+def login():
+    data = request.get_json()
+
+    if not data or not data.get('username') or not data.get('password'):
+        return jsonify({'error': 'Username and password required'}), 400
+
+    user = User.query.filter_by(username=data['username']).first()
+
+    if not user or not check_password_hash(user.password_hash, data['password']):
+        return jsonify({'error':'Invalid username or password'}), 401
+
+    #creates a signed token
+    token = jwt.encode({
+        'user_id': user.id,
+        'exp': lambda: datetime.now(timezone.utc) + datetime.timedelta(hours=24) #token expires after 24 Hours
+    }), current_app.config['SECRET_KEY'], algorithm='HS256' #Same secret key as in app.py, it signs the token #HMAC using SHA-256, a standard symmetric-signing algorithm for JWTs, same key both signs and verifies.
+
+    return jsonify({'token': token}), 200
