dc692e54b0a283cbb143ac8f57de8c135d8af3b8
Enaijite024 <220177245@aston.ac.uk>
2026-08-19 16:26:36 +0100

    Fix Error in  auth_middleware. Test task route commands

--- app.py
+++ app.py
@@ -1,13 +1,17 @@
 from flask import Flask
 from models import db
 from auth import auth_bp
+from tasks import tasks_bp
 
 app = Flask(__name__)#Application instance is created using the Flask class
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///tasks.db'#tells SQLAlchemy where the database is located, tasks.db in the current directory.
 app.config['SECRET_KEY'] = 'P@}K@y&ZKTfVYt@oXD['#secret key is used for securely signing the session cookie.
 
 db.init_app(app) #connects the Flask application to the SQLAlchemy database instance.
+#blueprint is a way to organize a group of related routes and handlers in a Flask application.
+#Allowing you to split your application into smaller and reusable components.
 app.register_blueprint(auth_bp)
+app.register_blueprint(tasks_bp)
 
 #creates the database tables based on the models in models.py.
 #This is done within the application context to ensure that the database operations are performed in the context of the Flask application.
--- auth_middleware.py
+++ auth_middleware.py
@@ -4,6 +4,8 @@ from flask import request, jsonify, current_app
 from models import User
 
 #decorator function that takes another function(f) as an argument
+#takes function, comfirms if user is authenticated by checking for a valid JWT token in the request headers 
+#then calls the original function(f) with the current user as an argument if the token is valid.
 def token_required(f):
     @wraps(f)#decorator that preserves the original function's metadata, such as its name, when it is wrapped by another function.
     def decorated(*args, **kwargs):
@@ -20,16 +22,16 @@ def token_required(f):
 
         #verifying token
         try:
-            payload = jwt.decode(token, current_app.CONFIG['SECRET_KEY'], algorithms=['HS256'])
-            current_user = User.query.get(payload['User_id'])
+            payload = jwt.decode(token, current_app.config['SECRET_KEY'], algorithms=['HS256'])
+            current_user = User.query.get(payload['user_id'])
             if not current_user:
                 return jsonify({'error':'User not found'}), 401
-        except jwt.ExpiredSignitureError:
+        except jwt.ExpiredSignatureError:
             return jsonify({'error':'Token has Expired'}), 401
         except jwt.InvalidTokenError:
             return jsonify({'error':'Invalid Token'}), 401
 
-        return f(current_user, *args **kwargs)
+        return f(current_user, *args, **kwargs)
 
     return decorated
             
